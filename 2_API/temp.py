e5931b99e9c6f2de5652074b21c4af90306cbd11
liliyakrnk <sofiakrivonosova@gmail.com>
2020-01-27 19:02:51 +0700

    add

---
+++ 2_API/temp.py
@@ -0,0 +1,19 @@
+import requests
+
+geocoder_request = "http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode=Уфа&format=json"
+
+# Выполняем запрос.
+response = requests.get(geocoder_request)
+if response:
+    # Преобразуем ответ в json-объект
+    json_response = response.json()
+
+    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+    toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
+
+    country, subject, siti = toponym_address
+    print(toponym_address)
+else:
+    print("Ошибка выполнения запроса:")
+    print(geocoder_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
