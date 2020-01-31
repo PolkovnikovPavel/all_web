d9b16bdd136e4ac28d27cfcb31a98ede262f1eca
Павел <pavelpolkovnikov334@gmail.com>
2020-01-31 21:12:48 +0700

    add task "Телефоны наносят ответный удар"

---
+++ 3_исключения/Телефоны наносят ответный удар.py
@@ -0,0 +1,66 @@
+class num_Exception(Exception):
+    pass
+
+
+codes = [(910, 919), (980, 989), (920, 939), (902, 906), (960, 969)]
+
+operator_codes = []
+for code in codes:
+    operator_codes.extend(range(code[0], code[1] + 1))
+
+
+def check_num(num):
+    num = ''.join(num.split())
+    if num[0] != '8' and num[:2] != '+7':
+        raise num_Exception('неверный формат')
+
+    count = 0
+    for s in num:
+        if s == '-':
+            if num.index(s) == 0 or num.index(s) == len(num) - 1:
+                raise num_Exception('неверный формат')
+            count += 1
+            if count == 2:
+                raise num_Exception('неверный формат')
+        else:
+            count = 0
+
+    count = 0
+    for s in num:
+        if s == '(':
+            count += 1
+            if count == 2:
+                raise num_Exception('неверный формат')
+        if s == ')':
+            count -= 1
+            if count < 0:
+                raise num_Exception('неверный формат')
+    if count != 0:
+        raise num_Exception('неверный формат')
+
+    bed_symbols = ['-', '(', ')']
+    result = ''
+    for s in num:
+        if s not in bed_symbols:
+            result += s
+    if result[0] != '+':
+        result = '+7' + result[1:]
+
+    if not result[1:].isdigit():
+        raise num_Exception('неверный формат')
+
+    if int(result[2:5]) not in operator_codes:
+        raise num_Exception('не определяется оператор сотовой связи')
+
+    if len(result) != 12:
+        raise num_Exception('неверное количество цифр')
+
+    return result
+
+
+try:
+    print(check_num(input()))
+except num_Exception as exc:
+    print(exc)
+
+
