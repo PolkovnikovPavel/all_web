71ec91b08db18f4d0aec5ac06daa6f11e91ff3bd
1234 <12345@gmail.com>
2020-02-15 10:32:05 +0700

    fix task "Поиск аптеки 2.0"

--- 6_HTTP/Поиск аптеки 2.0/Поиск аптеки 2.0.py
+++ 6_HTTP/Поиск аптеки 2.0/Поиск аптеки 2.0.py
@@ -1,28 +1,10 @@
 import sys
 import math
 from io import BytesIO
+from .distance import lonlat_distance
 
 import requests
 from PIL import Image
-
-
-def lonlat_distance(a, b):
-    degree_to_meters_factor = 111 * 1000 # 111 километров в метрах
-    a_lon, a_lat = a
-    b_lon, b_lat = b
-
-    # Берем среднюю по широте точку и считаем коэффициент для нее.
-    radians_lattitude = math.radians((a_lat + b_lat) / 2.)
-    lat_lon_factor = math.cos(radians_lattitude)
-
-    # Вычисляем смещения в метрах по вертикали и горизонтали.
-    dx = abs(a_lon - b_lon) * degree_to_meters_factor * lat_lon_factor
-    dy = abs(a_lat - b_lat) * degree_to_meters_factor
-
-    # Вычисляем расстояние между точками.
-    distance = math.sqrt(dx * dx + dy * dy)
-
-    return distance
 
 
 def get_params_for_static_maps_and_organization(json_response, start_point):
@@ -93,5 +75,4 @@ response = requests.get(map_api_server, params=map_params)
 
 
 print(get_data_of_organization(organization))
-
 Image.open(BytesIO(response.content)).show()
