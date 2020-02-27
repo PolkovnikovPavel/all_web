b3d4184a2245065ab8085b7c5b6cd6eca20a5856
Павел <pavelpolkovnikov334@gmail.com>
2020-02-27 23:59:03 +0700

    add task "Готовимся к миссии"

---
+++ 9_Patterns/Готовимся к миссии/Готовимся к миссии.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def home(title):
+    user = "Ученик Яндекс.Лицея"
+    return render_template('base.html', title=title,
+                           username=user)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
