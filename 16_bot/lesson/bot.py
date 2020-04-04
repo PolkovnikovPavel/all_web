b72a8403a5cbae884e84fc75af067a177d3bc161
Павел <pavelpolkovnikov334@gmail.com>
2020-04-04 14:51:43 +0700

    add "Бот Дата-Время"

--- 16_bot/lesson/bot.py
+++ 16_bot/lesson/bot.py
@@ -1,12 +1,18 @@
 import random
+import datetime
 
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 
 def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message,
-                                'random_id': random.randint(0, 2048)})
+    vk.messages.send(user_id=user_id,
+                     message=message,
+                     random_id=random.randint(0, 2 ** 64))
+
+
+week   = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница',  'суббота', 'воскресенье']
+key_words_time = ['врем', 'число', 'дата', 'день', 'врем', 'час']
 
 
 # API-ключ созданный ранее
@@ -23,19 +29,26 @@ for event in longpoll.listen():
 
     if event.type == VkEventType.MESSAGE_NEW:
         if event.to_me:
-            print(event)
-            print('Новое сообщение:')
-
             vk = vk_session.get_api()
             response = vk.users.get(user_id=event.user_id)
-            print('Для меня от:', response)
 
+            print(event)
+            print('Новое сообщение:')
+            print('Для меня от:', response)
             print('Текст:', event.text)
-            str = f"{response[0]['first_name']} {response[0]['last_name']} это вы? Я про вас всё знаю!! ахахаха!!!"
+
+            str = f"{response[0]['first_name']} {response[0]['last_name']} это вы? Я про вас всё знаю!! ахахаха!\n\nВы можете у меня спросить время"
             if event.text.lower() == 'нет':
                 str = 'Не ври'
             elif event.text.lower() == 'ладно':
                 str = 'Я же говорил НЕ ПИСАТь'
-            vk.messages.send(user_id=event.user_id,
-                             message=str,
-                             random_id=random.randint(0, 2 ** 64))
+            elif any(map(lambda x: x in event.text.lower(), key_words_time)):
+                moscow_time = datetime.datetime.now()
+                date = f'{moscow_time.year}-{moscow_time.month}-{moscow_time.day}'
+                time = f'{moscow_time.hour}:{moscow_time.minute}:{moscow_time.second}'
+                weekday = week[moscow_time.weekday()]
+
+                str = f"дата: {date}\nвремя: {time}\nдень недели: {weekday}"
+
+            write_msg(event.user_id, str)
+
